5b3d8b2820f48f9bd149f4f1a4c7bcb64b450ecd
yusun-lab <yu.sun@hyperisland.se>
2025-11-18 23:19:57 +0100

    5 is done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,29 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ playerOne.name }} - {{ playerOne.age }}</p>
-    <button @click="updatePlayerOne">Update player one</button>
-
-    <h2>Reactive</h2>
-    <p>{{ playerTwo.name }} - {{ playerTwo.age }} - {{ nameTwo }}</p>
-    <button @click="updatePlayerTwo">Update player two</button>
+    <input type="text" v-model="search" />
+    <p>search term - {{ search }}</p>
+    <ul>
+      <li v-for="name in matchingNames" :key="name">{{ name }}</li>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from "vue";
+import { computed, ref } from "vue";
 
-const playerOne = ref({ name: "Haaland", age: 24 });
-const playerTwo = reactive({ name: "Mbappé", age: 25 });
+const search = ref("");
+const names = ref([
+  "Haaland",
+  "Mbappé",
+  "Bellingham",
+  "Yamal",
+  "Zabiri",
+  "Vinicius",
+  "Dembele",
+]);
 
-const nameOne = ref("Haaland");
-const nameTwo = reactive("Mbappé"); // it's a  string, primitive value, not an object. We can't use primitive values with reactive.
-
-const updatePlayerOne = () => {
-  playerOne.value.age = 58;
-};
-
-const updatePlayerTwo = () => {
-  playerTwo.age = 59;
-  nameTwo = reactive("Yamal"); // nameTwo is a constant, it can't be changed.
-};
+const matchingNames = computed(() => {
+  return names.value.filter((name) => name.includes(search.value));
+});
 </script>
