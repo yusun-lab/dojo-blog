48e47f4273bcba76f8fd55871ce8ae346df31b82
yusun-lab <yu.sun@hyperisland.se>
2025-11-18 11:31:53 +0100

    1 is done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="home">Home</div>
+  <p>My name is {{ name }} and my age is {{ age }}</p>
+  <button @click="handleClick">Click me</button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+console.log("setup");
+
+const name = ref("mario");
+const age = ref(30);
+
+const handleClick = () => {
+  console.log("button clicked");
+};
+</script>
