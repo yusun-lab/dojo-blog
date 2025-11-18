4a456c435c2649110a9268370036765e59357c8b
yusun-lab <yu.sun@hyperisland.se>
2025-11-18 17:58:23 +0100

    3 is done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,8 @@
   <div class="home">Home</div>
   <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
   <button @click="handleClick">Click me</button>
+  <button @click="age++">add 1 to age</button>
+  <input type="text" v-model="name" />
 </template>
 
 <script setup lang="ts">
@@ -9,25 +11,15 @@ import { ref } from "vue";
 
 console.log("setup");
 
-const p = ref<HTMLParagraphElement | null>(null);
-// p.value is null here because:
-// 1. setup() runs before the component is mounted
-// 2. DOM elements are created and bound to refs only after mounting
-// 3. The template's ref="p" will bind to this ref only when the component is mounted
-// 4. Use onMounted() if you need to access DOM elements during initialization
-console.log(p, p.value);
+// const p = ref<HTMLParagraphElement | null>(null);
 
-const name = ref("mario");
+const name = ref("mario"); // const doesn't mean the value can't be changed, it just means the reference object is a constant and can't be changed
 const age = ref(30);
 
 const handleClick = () => {
-  console.log(p, p.value); // p.value is not null here because:
-  // 1. handleClick() runs after the component is mounted
-  // 2. DOM elements are created and bound to refs only after mounting
-  // 3. The template's ref="p" will bind to this ref only when the component is mounted
-  p.value?.classList.add("test");
-  if (p.value) {
-    p.value.textContent = "hello, world";
-  }
+  name.value = "luigi";
+  age.value = 40;
 };
+
+const score = ref(5);
 </script>
