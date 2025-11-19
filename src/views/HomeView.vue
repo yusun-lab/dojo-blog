55106988cbc9e337b94660577046ab18b42f2a89
yusun-lab <yu.sun@hyperisland.se>
2025-11-19 10:19:07 +0100

    6 is done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,15 @@
     <h1>Home</h1>
     <input type="text" v-model="search" />
     <p>search term - {{ search }}</p>
-    <ul>
+    <ol>
       <li v-for="name in matchingNames" :key="name">{{ name }}</li>
-    </ul>
+    </ol>
+    <button @click="handleClick">stop watching</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed, ref, watch, watchEffect } from "vue";
 
 const search = ref("");
 const names = ref([
@@ -23,7 +24,24 @@ const names = ref([
   "Dembele",
 ]);
 
+// watch() returns a stop function that can be called to stop watching
+// The variable name 'stopwatch' is arbitrary - you could name it anything
+const stopwatch = watch(search, () => {
+  console.log("watch function ran");
+});
+
+// watchEffect() also returns a stop function that can be called to stop the effect
+// The variable name 'cleanupEffect' is arbitrary - you could name it anything
+const cleanupEffect = watchEffect(() => {
+  console.log("watch effect function ran", search.value);
+});
+
 const matchingNames = computed(() => {
   return names.value.filter((name) => name.includes(search.value));
 });
+
+const handleClick = () => {
+  stopwatch(); // stops the watch function - permanently stops the watcher
+  cleanupEffect(); // stops the watch effect function - permanently stops the effect
+};
 </script>
